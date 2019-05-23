4d25e05d2d2229c8e3d49dfefcad6619212f59c4
CadillacJenkins <walter.k.jenkins@gmail.com>
2019-05-22 22:39:34 -0500

    agol map with basemap gallery example from tut

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 require([
     "esri/Map",
     "esri/views/MapView",
-    "esri/Graphic",
-    "esri/geometry/Point",
-    "esri/symbols/SimpleMarkerSymbol",
-  ], function(Map, MapView) {
+    "esri/widgets/BasemapToggle", 
+    "esri/widgets/BasemapGallery"
 
-    var map = new Map({
-      basemap: "topo-vector"
+  ], function(Map, MapView, BasemapToggle, BasemapGallery) {
+
+    const map = new Map({
+        basemap: "dark-gray-vector"
     });
 
-    var view = new MapView({
-      container: "viewDiv",
-      map: map,
-      center: [-90.242347,38.597015],
-      zoom: 15
+    const view = new MapView({
+        container: "viewDiv",
+        map: map,
+        center: [-90.242347,38.597015],
+        zoom: 15
     });
-    var point = {
+    const baseTog = new BasemapToggle({ // basemap toggle
+        view: view,
+        nextBasemap: "satellite",
+    });
+    const baseGall = new BasemapGallery({
+        view: view,
+        source: {
+            portal:{
+                url: "https://www.arcgis.com", 
+                useVectorBasemaps: true
+            }
+        }
+    });
+    const point = {
         type: "point",
         x: -90.242347, 
         y: 38.597015
@@ -31,6 +44,11 @@ require([
         geometry: point,
         symbol: markerSymbol
     };
+    //view.ui.add(baseTog, "top-right");
+
+    view.ui.add(baseGall, {
+        position: "top-right"
+    });
     view.graphics.add(pointGraphic);
 
   });

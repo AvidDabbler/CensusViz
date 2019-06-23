4bed8897df635e1fdbe93a10e161bfa32f4a4370
CadillacJenkins <walter.k.jenkins@gmail.com>
2019-06-22 19:30:14 -0500

    update to radioFunction

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+
+
+
+const radioFunction = (function(){
+        if(document.getElementById("pop").checked){
+            console.log("population");
+        }
+        else if(document.getElementById("vacant").checked){
+            console.log("vacant");
+        }    
+        else if(document.getElementById("owner").checked){
+            console.log("owner");
+        }    
+        else if(document.getElementById("renter").checked){
+            console.log("renter");
+        }    
+        else if(document.getElementById("black").checked){
+            console.log("black");
+        }    
+        else{
+            console.log("blank");
+        }
+    });
+
+console.log("test");
+
 require([
     "esri/Map",
     "esri/views/MapView",
@@ -21,33 +47,17 @@ require([
     //ui
     const baseTog = new BasemapToggle({ // basemap toggle
         view: viewConfig,
-        nextBasemap: "satellite",
+        nextBasemap: "satellite"
     });
 
 
-    //point
-    const point = {
-        type: "point",
-        x: -90.242347, 
-        y: 38.597015
-    };
-    const markerSymbol = {
-        type: "simple-marker",
-        color: [255, 255, 255],
-        width: 1
-    };
-
-    const pointGraphic = {
-        type: "graphic",
-        geometry: point,
-        symbol: markerSymbol
-    };
+ 
     const defaultSym = {
         type: "simple-fill", // autocasts as new SimpleFillSymbol()
         outline: {
           // autocasts as new SimpleLineSymbol()
           color: [128, 128, 128],
-          width: "1px"
+          width: "0"
         }
     };
     const censusRender = { // https://developers.arcgis.com/javascript/latest/api-reference/esri-renderers-SimpleRenderer.html
@@ -75,20 +85,26 @@ require([
     };
     const feaLayPop = { // popup configuration
         "title": "Census Block Groups",
-        "content": "Population 2000: {pop2000} </br> Population 2007: {pop2007} </br> Population Density 2000: {pop00_sqmi} </br> Population Density: {pop07_sqmi} </br> Renter: {RENTER_OCC} </br> Owner: {OWNER_OCC}",
-        opacity: .7
+        "content": "Population 2000: {pop2000} </br> Population 2007: {pop2007} </br> Population Density 2000: {pop00_sqmi} </br> Population Density: {pop07_sqmi} </br> Renter: {RENTER_OCC} </br> Owner: {OWNER_OCC}"
     }
 
     const feaLay = new FeatureLayer({  // connects to the SimpleRenderer information
         url: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer/1",
         renderer: censusRender,  
-        popupTemplate: feaLayPop
+        popupTemplate: feaLayPop,
+        opacity: .4
+
     });
 
     // CALLS
     viewConfig.ui.add(baseTog, {
         position: "top-right"
     });
-    viewConfig.graphics.add(pointGraphic);
     mapConfig.add(feaLay);
   });
+
+  window.onload = function() {
+    radioFunction();
+    console.log("test2");
+
+};

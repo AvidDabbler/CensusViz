4c0b59ba7864545d5878a637db7cd20359be0148
CadillacJenkins <walter.k.jenkins@gmail.com>
2019-07-20 15:08:06 -0500

    updated buttons, logic and styling

--- script.js
+++ script.js
@@ -1,110 +1,225 @@
 
-
-
-const radioFunction = (function(){
-        if(document.getElementById("pop").checked){
-            console.log("population");
+//! INITIAL STYLEING FOR MAP
+const defaultSymb = {//! OUTLINE PARAMETER FOR layDef
+    type: "simple-fill", // autocasts as new SimpleFillSymbol()
+    outline: {
+        // autocasts as new SimpleLineSymbol()
+        color: [128, 128, 128],
+        width: "0"
         }
-        else if(document.getElementById("vacant").checked){
-            console.log("vacant");
-        }    
-        else if(document.getElementById("owner").checked){
-            console.log("owner");
-        }    
-        else if(document.getElementById("renter").checked){
-            console.log("renter");
-        }    
-        else if(document.getElementById("black").checked){
-            console.log("black");
-        }    
-        else{
-            console.log("blank");
-        }
-    });
-
-console.log("test");
-
-require([
-    "esri/Map",
-    "esri/views/MapView",
-    "esri/widgets/BasemapToggle", 
-    "esri/layers/FeatureLayer",
-
-  ], function(Map, MapView, BasemapToggle, FeatureLayer) {
-    
-    //config base map
-    const mapConfig = new Map({
-        basemap: "dark-gray-vector",
-    });
-
-    const viewConfig = new MapView({
-        container: "viewDiv",
-        map: mapConfig,
-        center: [-90.242347, 38.597015],
-        zoom: 10
-    });
-
-    //ui
-    const baseTog = new BasemapToggle({ // basemap toggle
-        view: viewConfig,
-        nextBasemap: "satellite"
-    });
-
-
- 
-    const defaultSym = {
-        type: "simple-fill", // autocasts as new SimpleFillSymbol()
-        outline: {
-          // autocasts as new SimpleLineSymbol()
-          color: [128, 128, 128],
-          width: "0"
-        }
-    };
-    const censusRender = { // https://developers.arcgis.com/javascript/latest/api-reference/esri-renderers-SimpleRenderer.html
-
-        type: "simple",
-        symbol: defaultSym,
-        label: "Blockgroups",
-        visualVariables: [
-            {
-                type: "color",
-                field: "pop07_sqmi",
-                stops: [
-                    {
-                        value:1000,
-                        color: "#FFFCD4",
-                    },
-                    {
-                        value:17000,
-                        color: "#350242",
-                    }
-                ]
-            }
-        ]
-
-    };
-    const feaLayPop = { // popup configuration
-        "title": "Census Block Groups",
-        "content": "Population 2000: {pop2000} </br> Population 2007: {pop2007} </br> Population Density 2000: {pop00_sqmi} </br> Population Density: {pop07_sqmi} </br> Renter: {RENTER_OCC} </br> Owner: {OWNER_OCC}"
-    }
-
-    const feaLay = new FeatureLayer({  // connects to the SimpleRenderer information
-        url: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer/1",
-        renderer: censusRender,  
-        popupTemplate: feaLayPop,
-        opacity: .4
-
-    });
-
-    // CALLS
-    viewConfig.ui.add(baseTog, {
-        position: "top-right"
-    });
-    mapConfig.add(feaLay);
-  });
-
-  window.onload = function() {
-    radioFunction();
-    console.log("test2");
-
 };
+
+
+const symb = {//! LAYER CHOICES
+    population:{ 
+        style:{
+            type: "simple",
+            symbol: defaultSymb,
+            label: "Blockgroups",
+            visualVariables: [
+                {
+                    type: "color",
+                    field: "pop07_sqmi",
+                    stops: [
+                        {
+                            value:1000,
+                            color: "#FFFCD4",
+                        },
+                        {
+                            value:17000,
+                            color: "#350242",
+                        }
+                    ]
+                }
+            ]},
+        popup:{
+            "title": "Census Block Groups",
+            "content": "Population 2000: {pop2000} </br> Population 2007: {pop2007} </br> Population Density 2000: {pop00_sqmi} </br> Population Density: {pop07_sqmi} </br> Renter: {RENTER_OCC} </br> Owner: {OWNER_OCC}"
+        }
+
+    },
+    senior:{ 
+        style:{
+            type: "simple",
+            symbol: defaultSymb,
+            label: "Blockgroups",
+            visualVariables: [
+                {
+                    type: "color",
+                    field: "AGE_65_UP",
+                    stops: [
+                        {
+                            value:100,
+                            color: "#FCDFEB",
+                        },
+                        {
+                            value:1000,
+                            color: "#34091C",
+                        }
+                    ]
+                }
+            ]},
+        popup:{
+            "title": "Senior Block Groups",
+            "content": "Population 2000: {pop2000} </br> Population 2007: {pop2007} </br> Population Density 2000: {pop00_sqmi} </br> Population Density: {pop07_sqmi} </br> Renter: {RENTER_OCC} </br> Owner: {OWNER_OCC}"
+        }
+    },
+    hisp:{ 
+        style:{
+            type: "simple",
+            symbol: defaultSymb,
+            label: "Blockgroups",
+            visualVariables: [
+                {
+                    type: "color",
+                    field: "HISPANIC",
+                    stops: [
+                        {
+                            value:10,
+                            color: "#FFE5DC",
+                        },
+                        {
+                            value:100,
+                            color: "#341509",
+                        }
+                    ]
+                }
+            ]},
+        popup:{ 
+            "title": "Hispanic Block Groups",
+            "content": "Population 2000: {pop2000} </br> Population 2007: {pop2007} </br> Population Density 2000: {pop00_sqmi} </br> Population Density: {pop07_sqmi} </br> Renter: {RENTER_OCC} </br> Owner: {OWNER_OCC}"
+        }
+    },
+    renter:{ 
+        style:{
+            type: "simple",
+            symbol: defaultSymb,
+            label: "Blockgroups",
+            visualVariables: [
+                {
+                    type: "color",
+                    field: "RENTER_OCC",
+                    stops: [
+                        {
+                            value:50,
+                            color: "#FFFCD4",
+                        },
+                        {
+                            value:1000,
+                            color: "#350242",
+                        }
+                    ]
+                }
+            ]},
+        popup:{ 
+            "title": "Senior Block Groups",
+            "content": "Population 2000: {pop2000} </br> Population 2007: {pop2007} </br> Population Density 2000: {pop00_sqmi} </br> Population Density: {pop07_sqmi} </br> Renter: {RENTER_OCC} </br> Owner: {OWNER_OCC}"
+        }
+    },
+    vac:{ 
+        style:{
+            type: "simple",
+            symbol: defaultSymb,
+            label: "Blockgroups",
+            visualVariables: [
+                {
+                    type: "color",
+                    field: "VACANT",
+                    stops: [
+                        {
+                            value:10,
+                            color: "#EBF7E3",
+                        },
+                        {
+                            value:500,
+                            color: "#1B3409",
+                        }
+                    ]
+                }
+            ]},
+        popup:{ 
+            "title": "Senior Block Groups",
+            "content": "Population 2000: {pop2000} </br> Population 2007: {pop2007} </br> Population Density 2000: {pop00_sqmi} </br> Population Density: {pop07_sqmi} </br> Renter: {RENTER_OCC} </br> Owner: {OWNER_OCC}"
+        }
+    },
+};
+
+let layDef =symb.population;
+
+const config =(stat)=>{//! ONCLICK LOGIC AND RENDER PROPERTIES FOR DIFFERENT DEMOGRAPHICS
+
+
+    switch(stat) {
+        case `pop`:
+            layDef =symb.population;
+            init();
+            break;
+        case `senior`:
+            layDef =symb.senior;
+            init();
+            break;
+        case `hisp`:
+            layDef =symb.hisp;
+            init();
+            break;
+        case `renter`:
+            layDef =symb.renter;
+            init();
+            break;
+        case `vac`:
+            layDef =symb.vac;
+            init();
+            break;
+        default:
+            console.log(`clkFunct error`)
+        }    
+};
+
+
+const init = (()=>{//! ARCGIS API 4.11 INTITIALIZATION AND MAP FUNCTIONALITY
+    require([
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/widgets/BasemapToggle", 
+        "esri/layers/FeatureLayer"
+
+    ],(Map, MapView, BasemapToggle, FeatureLayer)=>{
+        //config base map
+        const mapConfig = new Map({//! SET UP OF INITITAL BASEMAP
+            basemap: "dark-gray-vector",
+        });
+
+        const viewConfig = new MapView({//! DEFAULT VIEW CONFIG
+            container: "viewDiv",
+            map: mapConfig,
+            center: [-90.242347, 38.597015],
+            zoom: 10
+        });
+
+        //ui
+        const baseTog = new BasemapToggle({ //! BASEMAP TOGGLE FUNCTIONALITY 
+            view: viewConfig,
+            nextBasemap: "satellite"
+        });
+        const feaLay = new FeatureLayer({  //! LINK TO LAYER, STYLING AND POPUP
+            url: "https://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer/1",
+            renderer: layDef.style,  
+            popupTemplate: layDef.popup,
+            opacity: .4
+
+        })
+        // ARCGIS API CALLS
+        viewConfig.ui.add(baseTog, {
+            
+            position: "top-right"
+        })
+        mapConfig.add(feaLay);
+
+        window.onload = function() {};
+        return{
+            layShift: mapConfig.add(feaLay),
+        };
+    });
+});
+
+init();
